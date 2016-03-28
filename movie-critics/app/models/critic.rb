dc7325a3707766ea5937aa93bb386733c46a50e1
Ian Kinner & Menuka Samaranayake <pair+ian-kinner.menuka-s@devbootcamp.com>
2016-03-28 15:25:43 -0500

    add to user show partials, fix some associations, add events route

--- movie-critics/app/models/critic.rb
+++ movie-critics/app/models/critic.rb
@@ -1,6 +1,7 @@
 class Critic < ActiveRecord::Base
-  has_many :events
+  has_many :joined_events, through: :reviews, source: :event
   has_many :reviews
+  has_many :created_events, class_name: "Event", foreign_key: :creator_id
   has_many :reviewed_movies, through: :reviews, source: :movie
 
   has_secure_password
