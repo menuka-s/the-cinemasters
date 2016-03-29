32e50616d3cbd80911e187f4b6091f4f9ad305b3
ian-kinner <ian.kinner@gmail.com>
2016-03-29 09:26:33 -0500

    Fix review update / edit, review controller, event show

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,21 +8,26 @@ class ReviewsController < ApplicationController
   end
 
   def new
+    ### This should just create a new review with movie_id and critic_id - no form needed
+    ### content and rating should be set to "" **not nil!
+    @labels = ["Rubric 1","Rubric 2","Rubric 3","Rubric 4","Rubric 5","Rubric 6","Rubric 7","Rubric 8","Rubric 9"]
     @review = Review.new(event_id: params[:event_id])
   end
 
   def edit
-     @review = Review.find(params[:id])
+    @labels = ["Rubric 1","Rubric 2","Rubric 3","Rubric 4","Rubric 5","Rubric 6","Rubric 7","Rubric 8","Rubric 9"]
+    @review = Review.find(params[:id])
   end
 
   def create
+
+### if date > publish_date, render toolate
 
     rubrics_string = rubrics_to_string(params[:review])
 
     @review = Review.new({content: params[:review][:content], ratings: rubrics_string})
     @review.critic = session[:user_id]
     if @review.save
-      session[:user_id] = @review.id
       redirect_to @review
     else
       render "review/new"
@@ -30,6 +35,14 @@ class ReviewsController < ApplicationController
   end
 
   def update
+    rubrics_string = rubrics_to_string(params[:review])
+    @review = Review.find(params[:id])
+      @review.update_attributes({content: params[:review][:content], ratings: rubrics_string})
+    if @review.save
+      redirect_to @review.event
+    else
+      render "review/edit"
+    end
   end
 
   def destroy
@@ -40,7 +53,8 @@ class ReviewsController < ApplicationController
   def rubrics_to_string(input)
     string = ""
     9.times do |i|
-      string += input["rubric" + i.to_s] + ","
+      puts "\n\n\n\n\n\n#{input}\n\n\n\n\n\n"
+      string += input["rubric" + (i+1).to_s] + ","
     end
     return string
   end
