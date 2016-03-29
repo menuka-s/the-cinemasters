bb0cc8f7224fe17a28e6794b56c20bcd2d3f8719
Ian Kinner & Menuka Samaranayake <pair+ian-kinner.menuka-s@devbootcamp.com>
2016-03-29 14:32:48 -0500

    Add Rspec tests for all models

--- spec/models/critic_spec.rb
+++ spec/models/critic_spec.rb
@@ -2,9 +2,36 @@ require 'rails_helper'
 
 describe Critic do
   let(:critic) { Critic.new(username: "Camel Cayce", password: "password", email: "donttellmomthebabysittersisdead@gmail.com") }
-  describe "it has a password" do
-    it "should have a password" do
-      expect(critic.password_digest).to_not be_nil
+
+  context "username" do
+    it "should have a username" do
+      expect(critic.username).to_not be_nil
+    end
+
+    it "should have the correct username" do
+      expect(critic.username).to eq "Camel Cayce"
     end
   end
+
+  context "password" do
+    it "should have a password" do
+      expect(critic.password).to_not be_nil
+    end
+
+    it "should have the correct password" do
+      expect(critic.password).to eq "password"
+    end
+  end
+
+  context "email" do
+    it "should have an email" do
+      expect(critic.email).to_not be_nil
+    end
+
+
+    it "should have the correct email" do
+      expect(critic.email).to eq "donttellmomthebabysittersisdead@gmail.com"
+    end
+  end
+
 end
