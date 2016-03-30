3c7b49292524f0f24348b9fae5b357e7a9bf9b7c
Prince Sadie <princesadie@gmail.com>
2016-03-30 14:12:50 -0500

    Add changes

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,11 +1,11 @@
 module EventsHelper
   def event_is_editable?(event)
-    return true if event.pub_date > Date.today
+    return true if event && event.pub_date > Date.today
     return false
   end
 
   def event_is_publishible?(event)
-    return true if event.pub_date <= Date.today
+    return true if event && event.pub_date <= Date.today
     return false
   end
 end
