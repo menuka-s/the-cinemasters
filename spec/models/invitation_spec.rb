43ccea3dd9013a7698f79c4c4fba41dcea7b5222
Rin <aaronopsahl@gmail.com>
2016-03-30 13:34:29 -0500

    Add invitations model spec and routes rspec edits

--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Invitation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	let(:invitation) { Invitation.new(code: "Xsh29did0d3kd0", critic: Critic.new(id: 1)) }
+  context "code" do
+  	it "should exist" do
+  		expect(invitation.code).to_not be_nil
+  	end
+
+  	it "should have the correct code" do
+  		expect(invitation.code).to eq "Xsh29did0d3kd0"
+  	end
+  end
+
+  context "critic" do
+  	it "should exist" do
+  		expect(invitation.critic).to_not be_nil
+  	end
+
+  	it "should have the correct critic" do
+  		expect(invitation.critic.id).to eq 1 
+  	end
+  end
 end
