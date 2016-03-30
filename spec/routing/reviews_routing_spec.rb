f13d1bb441b78787d79e5f4f4a74d6f4eb6b0128
Rin <aaronopsahl@gmail.com>
2016-03-30 11:42:26 -0500

    Add rspec tests for all routes

---
+++ spec/routing/reviews_routing_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe ReviewsController, :type => :routing do
+  describe "routing" do
+
+    it "routes to #index" do
+      expect(:get => "/reviews").to route_to("reviews#index")
+    end
+
+    it "routes to #new" do
+      expect(:get => "/reviews/new").to route_to("reviews#new")
+    end
+
+    it "routes to #show" do
+      expect(:get => "/reviews/1").to route_to("reviews#show", :id => "1")
+    end
+
+    it "routes to #create" do
+      expect(:post => "/reviews").to route_to("reviews#create")
+    end
+
+    it "routes to #edit" do
+      expect(:get => "/reviews/1/edit").to route_to("reviews#edit", :id => "1" )
+    end
+
+    it "routes to #update" do
+      expect(:patch => "/reviews/1").to route_to("reviews#update", :id => "1") 
+    end
+
+    it "routes to #destroy" do
+      expect(:delete => "/reviews/1").to route_to("reviews#destroy", :id => "1")
+    end
+
+    it "routes to #draftsave" do
+      expect(:post => "/reviews/1/draftsave").to route_to("reviews#draftsave", :id => "1")
+    end
+
+    it "routes to #invite_handler" do 
+      expect(:post => "/reviews/invite_handler").to route_to("reviews#invite_handler")
+    end
+
+  end
+end
